214132c3eab94268582d833372a1f5adc0aa7d05
Arthur Franco <153031237+Ima23-d@users.noreply.github.com>
2026-04-23 23:55:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -269,4 +269,5 @@ def route_reenviar_codigo():
 
 # =================== RUN ===================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
